c20443a0ebd54fc6b5f9168ba970b5a4cf831402
bgblanch <bgblanch@wkbweb.com>
2020-08-25 14:00:52 -0500

    removed interpolation

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
-    name        = "${length(trimspace(var.name)) > 0 ? trimspace(var.name) : module.random_name.result}"
-    namespace   = "${length(trimspace(var.namespace)) > 0 ? trimspace(var.namespace) : ""}"
-    result      = "${format ("%s%s%s", local.name, var.separator, local.namespace)}"
+    name        = length(trimspace(var.name)) > 0 ? trimspace(var.name) : module.random_name.result
+    namespace   = length(trimspace(var.namespace)) > 0 ? trimspace(var.namespace) : ""
+    result      = format ("%s%s%s", local.name, var.separator, local.namespace)
 }
 
 module "random_name" {
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "namespace_result" {
     description     = "The combination of name and namespace using the specified string format"
-    value           = "${local.result}"
+    value           = local.result
 }
