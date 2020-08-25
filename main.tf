02148a2b928e020d29e268d9c9b80284219b8e6b
bgblanch <bgblanch@wkbweb.com>
2020-08-25 10:42:23 -0500

    misspelled trimspace

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
     name        = "${length(trimspace(var.name)) > 0 ? trimspace(var.name) : module.random_name.result}"
-    namespace   = "${length(trimspace(var.namespace)) > 0 ? trimspae(var.namespace) : ""}"
+    namespace   = "${length(trimspace(var.namespace)) > 0 ? trimspace(var.namespace) : ""}"
     result      = "${format ("%s%s%s", local.name, var.separator, local.namespace)}"
 }
 
